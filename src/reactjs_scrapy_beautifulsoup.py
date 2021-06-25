8cd83559b1323ff4a7c326640bee690ed92a2dce
sntwandy <sntwandy@gmail.com>
2021-06-25 11:55:51 -0400

    ReactJS Doc scraping

---
+++ src/reactjs_scrapy_beautifulsoup.py
@@ -0,0 +1,27 @@
+from scrapy.item import Field
+from scrapy.item import Item
+from scrapy.selector import Selector
+from scrapy.spiders import Spider
+from scrapy.loader import ItemLoader
+
+
+class ReactJSDoc(Item):
+    title = Field()
+
+
+class ReactJSDocumentation(Spider):
+    name = 'ReactJSSpider'
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+    }
+    start_urls = ['https://reactjs.org/docs/getting-started.html']
+
+    def parse(self, response):
+        sel = Selector(response)
+        doc_list_titles = sel.xpath('//div[@class="css-15weewl"]//h3')
+
+        for doc in doc_list_titles:
+            item = ItemLoader(ReactJSDoc(), doc)
+            item.add_xpath('title', './text()')
+
+            yield item.load_item()
